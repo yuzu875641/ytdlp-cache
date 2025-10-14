72a502de312a610cf5e69850cd8d1713379b1e61
ゆず <f.kokori85@gmail.com>
2025-10-14 13:16:00 +0900

    Create cache.js

---
+++ cache.js
@@ -0,0 +1,13 @@
+import { LRUCache } from 'lru-cache';
+
+const options = {
+  max: 500,
+  ttl: 1000 * 60 * 60, 
+  allowStale: false,
+  updateAgeOnGet: false,
+  updateAgeOnHas: false,
+};
+
+const cache = new LRUCache(options);
+
+export default cache;
