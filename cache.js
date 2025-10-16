0e735a7091c67e4d2b09ce05317b6687911a36c8
ゆず <f.kokori85@gmail.com>
2025-10-16 21:34:32 +0900

    Update cache TTL from 1 hour to 4 hours

--- cache.js
+++ cache.js
@@ -1,9 +1,9 @@
 import { LRUCache } from 'lru-cache';
 
-// キャッシュの設定: 1時間 (1000ミリ秒 * 60秒 * 60分)
+// キャッシュの設定: 4時間 (1000ミリ秒 * 60秒 * 60分 * 4時間)
 const options = {
   max: 500,
-  ttl: 1000 * 60 * 60, 
+  ttl: 1000 * 60 * 60 * 4, // 14,400,000 ms
   allowStale: false,
   updateAgeOnGet: false,
   updateAgeOnHas: false,
